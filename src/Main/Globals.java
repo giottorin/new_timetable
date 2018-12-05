ce2d0f9613c717e4b3cab665d0627071be9e630b
gerjich <44424667+gerjich@users.noreply.github.com>
2018-12-05 13:39:21 +0500

    Есть чтение файлов из папки FileNames. Но с ошибкой при выполнении Chat.class

--- src/Main/Globals.java
+++
@@ -1,7 +0,0 @@
-package Main;
-
-public class Globals {
-    public int Id = 0;// todo private
-
-}
-
--- src/Main/User.java
+++
@@ -1,8 +0,0 @@
-package Main;
-
-public class User {
-    public String name = new String();
-    Globals glob = new Globals();
-    public final int id = ++glob.Id;
-
-}
